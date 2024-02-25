4bc36d199f52a54c48a326dd7c1d298a4bc35205
hamidreza abedi <92696830+hamidrabedi@users.noreply.github.com>
2024-02-25 20:30:34 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=[],
+    keywords=['python', 'cache', 'memory', 'database', 'pyinmem', 'in memory database'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
